77df47d0e256848c2bebe4a3fbd31d86ae3e869a
Gonzalo Duran <gon.duran@duocuc.cl>
2024-04-07 16:59:46 -0400

    entrega experiencia 2 semana 5 fullstack I, inclusion CRUD

--- peliculas/src/main/java/com/example/peliculas/controller/PeliculaController.java
+++ peliculas/src/main/java/com/example/peliculas/controller/PeliculaController.java
@@ -2,6 +2,7 @@ package com.example.peliculas.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequestMapping("/peliculas")
@@ -51,4 +55,21 @@ public class PeliculaController {
             .body(e.getMessage());
     }
 
+    @PostMapping
+    public Pelicula createPelicula(@RequestBody Pelicula pelicula) {
+        System.out.println("Se ejecuta createPelicula.");
+        return peliculaService.createPelicula(pelicula);
+    }
+    
+    @PutMapping("/{id}")
+    public Pelicula updatePelicula(@PathVariable Long id, @RequestBody Pelicula pelicula) {
+        System.out.println("Se ejecuta updatePelicula " + id);
+        return peliculaService.updatePelicula(id, pelicula);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePelicula(@PathVariable Long id){
+        System.out.println("Se ejecuta deletePelicula " + id);
+        peliculaService.deletePelicula(id);
+    }
 }
